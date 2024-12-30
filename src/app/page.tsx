000cba34047e77f31f572f742feb1d0b7fd7c8f1
Nguyet Ha Phung <Nguyet-Ha1.Phung@stud.uni-regensburg.de>
2024-12-30 21:01:33 +0100

    customizing components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div>
-      <Button size="sm">Click me</Button>
-      <p className="text-red-500 font-semibold">Antonio</p>
+    <div className="flex gap-4">
+      <Input/>
+      <Button size='xs'>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant='destructive'>Destructive</Button>
+      <Button variant='ghost'>Ghost</Button>
+      <Button variant='muted'>Muted</Button>
+      <Button variant='outline'>Outline</Button>
+      <Button variant='teritary'>Teritary</Button>
     </div>
   );
 }
